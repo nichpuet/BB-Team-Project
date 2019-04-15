f30d4a390619f83084a2eb3957d062d5b27193c9
Thayen <thayburt@gmail.com>
2019-04-15 09:43:03 -0400

    Added Comment

--- BrickBreaker/Program.cs
+++ BrickBreaker/Program.cs
@@ -14,6 +14,7 @@ namespace BrickBreaker
         [STAThread]
         static void Main()
         {
+            //Comment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());

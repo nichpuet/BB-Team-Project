3208c01314ef9f6412e4bebbef4c1170e93d6667
kristiannahuber@gmail.com <kristiannahuber@gmail.com>
2019-04-29 09:43:08 -0400

    added powerup code

---
+++ BrickBreaker/Powerups.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BrickBreaker
+{
+    class Powerups
+    {
+        public int x, y, speed;
+        public string type;
+        public int width = 15;
+        public int height = 15;
+
+        public Powerups(int _x, int _y, int _speed, string _type)
+        {
+            x = _x;
+            y = _y;
+            speed = _speed;
+            type = _type;
+        }
+
+        public void powerupMove()
+        {
+            y += speed;
+        }
+
+        public bool PowerupCollision()
+        {
+            Rectangle paddleR = new Rectangle(GameScreen.paddle.x, GameScreen.paddle.y, GameScreen.paddle.width, GameScreen.paddle.height);
+            Rectangle powerupR = new Rectangle(x, y, width, height);
+
+            if (powerupR.IntersectsWith(paddleR))
+            {
+                //TODO this would activate the powerup
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
